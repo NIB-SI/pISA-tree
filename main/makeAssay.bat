eb24c97a8ffc03752a53a02ec733294714d464e2
ablejec <andrej.blejec@nib.si>
2016-12-12 23:33:34 +0100

    ad other types (RT, R, Stat)

--- main/makeAssay.bat
+++ main/makeAssay.bat
@@ -11,9 +11,9 @@ rem cd d:\_X
 rem Backup copy if assay folder exists
 rem robocopy %1 X-%1 /MIR
 rem ------------------------------------------------------
-echo ============================
+echo =================================
 echo pISA-tree: make ASSAY 
-echo ----------------------------
+echo ---------------------------------
 rem ----------------------------------------------
 rem Class: use argument 1 if present
 set mydate=%date:~13,4%/%date:~9,2%/%date:~5,2%
@@ -28,11 +28,20 @@ rem Ask for Class, loop if empty
 :Ask1
 if %IDClass% EQU "" set /p IDClass=Enter Assay Class [Wet/Dry]: 
 if %IDClass% EQU "" goto Ask1
+rem /I: case insensitive compare
+if /I %IDClass% EQU dry (set IDClass=Dry)
+if /I %IDClass% EQU d set IDClass=Dry
+if /I %IDClass% EQU wet set IDClass=Wet
+if /I %IDClass% EQU w set IDClass=Wet
+rem ----------------------------------------------
+rem Supported types
+if /I %IDClass% EQU Wet set "types=NGS / RT"
+if /I %IDClass% EQU Dry set "types=R / Stat"
 rem ----------------------------------------------
 rem Type: use argument 2 if present
 set IDType=""
 if "%2" EQU "" (
-set /p IDType=Enter Assay Type: 
+set /p "IDType=Enter Assay Type (%types%):" 
 ) else (
 set IDType=%2
 )
@@ -40,7 +49,7 @@ rem dir %IDType%* /B /AD
 rem Similar Assay IDs
 rem %IDType%* /AD
 :Ask2
-if %IDType% EQU "" set /p IDType=Enter Assay Type: 
+if %IDType% EQU "" set /p "IDType=Enter Assay Type (%types%):"
 if %IDType% EQU "" goto Ask2
 rem ----------------------------------------------
 rem ID : use argument 3 if present
@@ -74,13 +83,6 @@ goto Ask3
 REM Continue creating directory
 )
 rem ----------------------------------------------
-rem /I: case insensitive compare
-if /I %IDClass% EQU dry goto dry
-if /I %IDClass% EQU d goto dry
-if /I %IDClass% EQU wet goto wet
-if /I %IDClass% EQU w goto wet
-rem ----------------------------------------------
-
 rem Make new assay directory tree
 rem ----------------------------------------------
 :dry
@@ -157,12 +159,16 @@ echo ### ASSAY>> .\_ASSAY_DESCRIPTION.TXT
 echo Short Name:	%ID%>> .\_ASSAY_DESCRIPTION.TXT
 echo Assay Class:	 %IDClass%>> .\_ASSAY_DESCRIPTION.TXT
 rem ECHO ON
-  set analytesInput=Analytes.txt
+  rem set analytesInput=Analytes.txt
   rem if exist ../%analytesInput% ( copy ../%analytesInput% ./%analytesInput% )
   call:putMeta "Assay Title" aTitle *
   call:putMeta "Assay Description" aDesc *
-  if "%IDType%" == "NGS" goto NGS
-
+  goto %IDType%
+rem  if "%IDType%" == "NGS" goto NGS
+rem  if "%IDType%" == "RT" goto RT
+rem  if "%IDType%" == "R" goto R
+rem  if "%IDType%" == "Stat" goto Stat
+rem
 :NGS
 REM ------------------------------------------ NGS
   set analytesInput=Analytes.txt
@@ -178,11 +184,11 @@ REM ------------------------------------------ NGS
   call:putMeta2 "Date Isolation" a05 %mydate%
   call:putMeta2 "Storage RNA" a06 CU0369
   call:putMeta2 "Dnase treatment protocol" a7 *
-  call:putMeta2 "Dnase ID" a8 HT403_DNase XIDX_
+  call:putMeta2 "Dnase ID" a8 DNase XIDX_
   call:putMeta2 "Date DNAse_treatment" a9 %mydate%
   call:putMeta2 "Storage_DNAse_treated" a10 CU0370
-  call:putMeta2 "Operator" a11
-  call:putMeta2 "cDNA ID" a12 HT403_cDNA XIDX_
+  call:putMeta2 "Operator" a11 "*"
+  call:putMeta2 "cDNA ID" a12 cDNA XIDX_
   call:putMeta2 "DateRT" a13 %mydate%
   call:putMeta2 "Operator" a14 %a11%
   call:putMeta2 "Notes" a15 " "
@@ -192,7 +198,30 @@ REM ------------------------------------------ NGS
 REM
   goto Finish
 REM ---------------------------------------- /NGS
-REM ---------------------------------------- Next Assay Type
+:RT
+REM ---------------------------------------- RT
+  set analytesInput=Analytes.txt
+  if exist ..\%analytesInput% ( copy ..\%analytesInput% .\%analytesInput% )
+  set line1=
+  set line2=
+  call:putMeta2 "Dnase ID" a03	DNASE	XIDX_
+  call:putMeta2 "RTprotocol" a04 " "
+  call:putMeta2 "cDNA ID" a05 cDNA XIDX_
+  call:putMeta2 "DateRT" a06 %mydate%
+  call:putMeta2 "Operator" a07 *
+  
+  call:writeAnalytes %analytesInput% "%line1%" "%line2%"
+REM
+    goto Finish
+REM ---------------------------------------- /RT
+:R
+REM ---------------------------------------- R
+    goto Finish
+REM ---------------------------------------- /R
+:Stat
+REM ---------------------------------------- R
+    oto Finish
+REM ---------------------------------------- /R
 :Finish
 echo Data:	>> .\_ASSAY_DESCRIPTION.TXT
 rem ------------------------------------  include common.ini
@@ -207,7 +236,7 @@ rem copy existing files from nonversioned tree (if any)
 rem robocopy X-%ID% %ID% /E
 rem dir .\%ID% /s/b
 goto:eof
-rem
+rem ====================================== / makeAssay
 rem --------------------------------------------------------
 rem Functions
 :getInput   --- get text from keyboard
