3b5201873d44ffbfd4c5d932139474018002ecbf
ablejec <andrej.blejec@nib.si>
2018-07-24 13:23:31 +0200

    DO not create FST.TXT at investigation level

--- makeInvestigation.bat
+++ makeInvestigation.bat
@@ -71,7 +71,6 @@ echo # Investigation %ID% >  .\README.MD
 echo # Reports for investigation %ID% >  .\reports\README.MD
 echo # Presentations for investigation %ID% >  .\presentations\README.MD
 echo # Describe samples > .\phenodata.txt
-echo # Feature Summary Table> .\FST.txt
 rem
 setlocal EnableDelayedExpansion
 set LF=^
