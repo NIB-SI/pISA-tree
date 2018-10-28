ffc6bc720485aedee8a168d634f6328a0efcb1c6
ablejec <andrej.blejec@nib.si>
2018-10-29 00:26:13 +0100

    Update xcheckMetadata.bat

--- xcheckMetadata.bat
+++ xcheckMetadata.bat
@@ -19,30 +19,23 @@ rem change \ with /
 echo # Check for missing metadata >!lfn!
 echo %cd:\=  !LF!/%>>!lfn!
 For /F "tokens=1*" %%i in (src.tmp) do (
-rem (echo.|set /p =## %%i!LF!)>name.tmp
-rem copy !lfn!+line.tmp !lfn!
-echo !LF!---!LF!>>!lfn!
-rem copy !lfn!+name.tmp !lfn!
-rem Shorten the path( remove project root) and change \ to /
-set "fname=%%i"
-set "fname=!fname:%cd%= * !"
-set "fname=!fname:\=/!"
-(echo.|set /p =" !fname! !LF!")>>!lfn!
-echo !LF!---!LF!>>!lfn!
-REM set /p="TextHere" <nul >>!lfn!
-REM Add two blanks to each line
-set addtext="  "
-rem find stars
-for /f "delims=" %%a in ('findstr "*" %%i') do echo  ?? MISSING: %%a %addtext% >> !lfn!
-rem 
-if exist tmpfile.tmp del /q tmpfile.tmp
-rem for /f "delims=" %%l in (%%i) Do (
-rem      echo %%l %addtext% >> tmpfile.tmp
-rem )
-REM
-rem copy !lfn!+tmpfile.tmp !lfn!
-REM copy !lfn!+"%%i" !lfn!
-rem echo !LF! >> !lfn!
+	rem (echo.|set /p =## %%i!LF!)>name.tmp
+	rem copy !lfn!+line.tmp !lfn!
+	echo !LF!---!LF!>>!lfn!
+	rem copy !lfn!+name.tmp !lfn!
+	rem Shorten the path( remove project root) and change \ to /
+	set "fname=%%i"
+	set "fname=!fname:%cd%= * !"
+	set "fname=!fname:\=/!"
+	(echo.|set /p =" !fname! !LF!")>>!lfn!
+	echo !LF!---!LF!>>!lfn!
+	REM set /p="TextHere" <nul >>!lfn!
+	REM Add two blanks to each line
+	set addtext="  "
+	rem find stars
+	for /f "delims=" %%a in ('findstr "*" %%i') do echo  ?? MISSING: %%a %addtext% >> !lfn!
+	rem 
+	if exist tmpfile.tmp del /q tmpfile.tmp
 )
 echo !LF!---!LF!>>!lfn!
 del *.tmp
