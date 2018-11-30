09709e17b95d96b80180a67080d40f04e1af7069
ablejec <andrej.blejec@nib.si>
2018-11-30 12:50:23 +0100

    change x.lib search order p ... A

--- makeProject.bat
+++ makeProject.bat
@@ -1,6 +1,7 @@
 @ECHO OFF
 rem find root directory and call pISA routine refered by the file name
-for %%G in (..\..\..\..,..\..\..,..\..,..,.) do if exist %%G\Templates set "root=%%G" && goto:next
+for %%G in (.,..,..\..,..\..\..,..\..\..\..) do if exist %%G\Templates set "root=%%G" && goto:next
 :next
 call %root%\Templates\x.lib\pISA.cmd :pISA %~n0 %1 %2 %3 %4
+pause
 goto:EOF
