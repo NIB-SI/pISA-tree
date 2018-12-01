f89594cadf91c8957eb4664d0c5227e3be1d6762
ablejec <andrej.blejec@nib.si>
2018-12-01 21:32:24 +0100

    move batch content to library pISA.cmd

--- Templates/x.lib/pISA.cmd
+++ Templates/x.lib/pISA.cmd
@@ -851,8 +851,83 @@ del *.tmp
 @echo.
 @echo.Metadata of levels below %cd% are in !lfn!
 @echo.
+if %rtyp% EQU txt start excel !lfn! 
+if %rtyp% NEQ txt open !lfn!
+goto:eof
+rem XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+:xcheckMetadata
+@echo off
+rem -------------------------------------  pISA-tree %$ver%
+rem
+rem Check Metadata for levels below the current level
+rem ------------------------------------------------------
+rem Author: A Blejec <andrej.blejec@nib.si>
+rem (c) National Institute of Biology, Ljubljana, Slovenia
+rem 2016
+rem ------------------------------------------------------
+TITLE %$ver%
+setlocal EnableDelayedExpansion
+set lfn=xCheckMetadata.md
+set LF=^
+
+
+REM Two empty lines are necessary
+echo !LF!*!LF!>line.tmp
+where /R . _*.txt > src.tmp
+rem change \ with /
+echo # Check for missing metadata >!lfn!
+set "mycd=%cd:\=;%"
+set "mycd=%mycd:_=\_%
+echo %mycd:;=  !LF!/%>>!lfn!
+For /F "tokens=1*" %%i in (src.tmp) do (
+	rem (echo.|set /p =## %%i!LF!)>name.tmp
+	rem copy !lfn!+line.tmp !lfn!
+	echo !LF!---!LF!>>!lfn!
+	rem copy !lfn!+name.tmp !lfn!
+	rem Shorten the path( remove project root) and change \ to /
+	set "fname=%%i"
+	set "fname=!fname:%cd%= * !"
+	set "fname=!fname:\=/!"
+	set "fname=!fname:_=\_!"
+	echo !fname!
+	(echo.|set /p =" !fname! !LF!")>>!lfn!
+	echo !LF!---!LF!>>!lfn!
+	REM set /p="TextHere" <nul >>!lfn!
+	REM Add two blanks to each line
+	set addtext="  "
+	rem find stars
+	for /f "delims=" %%a in ('findstr "*" %%i') do echo  ?? MISSING: %%a %addtext% >> !lfn!
+	rem 
+	if exist tmpfile.tmp del /q tmpfile.tmp
+)
+echo !LF!---!LF!>>!lfn!
+del *.tmp
+@echo on
+rem type Metadata.md
+@echo Metadata levels below %cd%
 open !lfn!
-timeout 10
+goto:eof
+rem XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+:showTree
+@echo off
+rem -------------------------------------  pISA-tree %$ver%
+rem
+rem make a directory tree in TREE.txt
+rem ------------------------------------------------------
+rem Author: A Blejec <andrej.blejec@nib.si>
+rem (c) National Institute of Biology, Ljubljana, Slovenia
+rem 2016
+rem ------------------------------------------------------
+rem
+rem echo %~dp0 > d.tmp
+TITLE %$ver%
+echo %cd% > d.tmp
+tree /A /F > t.tmp
+copy d.tmp+t.tmp TREE.TXT
+del *.tmp
+type tree.txt
+echo. Directory tree file: TREE.TXT
+open tree.txt
 goto:eof
 rem XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 rem 
